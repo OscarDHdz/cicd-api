7138b8f0ef6b8e11e49274f52bf9ed64efb99649
Hernandez <o.a.hernandez@accenture.com>
2017-09-26 14:27:51 -0500

    Added Todo Test Suit

---
+++ server/test/schema.test.js
@@ -0,0 +1,98 @@
+var knex = require('../KnexDB.js');
+const expect = require('expect');
+var {Todo, TABLE_NAME} = require('../models/Todo');
+
+describe('SCHEMA TEST', function() {
+
+
+  var users = [];
+
+  const CLIENT = process.env.DB_CLIENT || 'sqlite3';
+
+  // Prepare input data
+  before(function (done) {
+
+    users = [
+      new Todo({
+        title: 'Todo #1',
+        body: 'Body del todo #1'
+      }),
+      new Todo({
+        title: 'Todo #2',
+        body: 'Este body deberia estar completado',
+        completed: false
+      })
+    ]
+    for (var i = 0; i < users.length; i++) {
+      delete users[i].id;
+    }
+
+    knex(TABLE_NAME).delete()
+    .then((res) => done())
+    .catch((err) => done(err))
+
+  })
+
+  describe('Todos schema', function() {
+
+    it('Should insert a todo', function (done) {
+      knex(TABLE_NAME).insert(users[0])
+      .then((res) => {
+        if ( CLIENT === 'sqlite3' ) users[0].id = res[0];
+        else users[0].id = res[0].id;
+        done();
+      })
+      .catch((err) => done(err))
+    })
+
+    it('Should insert a second todo', function (done) {
+      knex(TABLE_NAME).insert(users[1])
+      .then((res) => {
+        if ( CLIENT === 'sqlite3' ) users[1].id = res[0];
+        else users[1].id = res[0].id;
+        done()
+      })
+      .catch((err) => done(err))
+    })
+
+    it('Should retrive first inserted todo', function (done) {
+      knex(TABLE_NAME).select('*').where({id: users[0].id})
+      .then((res) => {
+        expect(res[0].id).toBe(users[0].id);
+        done()
+      })
+      .catch((err) => done(err))
+    })
+
+    it('Should set second inserted todo as completed', function (done) {
+      knex(TABLE_NAME).update({completed: true}).where({id: users[1].id})
+      .then((res) => {
+        return knex(TABLE_NAME).select('*').where({id: users[1].id})
+        done()
+      })
+      .then((res) => {
+        expect(new Boolean(res[0].completed).valueOf()).toEqual(true);
+        users[1].completed = true;
+        done();
+      })
+      .catch((err) => done(err))
+    })
+
+    it('Should delete first inserted todo as completed', function (done) {
+      knex(TABLE_NAME).del().where({id: users[0].id})
+      .then((res) => {
+        return knex(TABLE_NAME).select('*').where({id: users[0].id});
+      })
+      .then((res) => {
+        expect(res).toEqual([]);
+        done()
+      })
+      .catch((err) => done(err))
+    })
+
+
+
+  });
+
+
+})
